adc8a28746c465f096c8468c18a83cbf93c8960e
Rajan Patil <rpatil@equalexperts.com>
2017-03-26 20:51:22 +0100

    Added sample test

---
+++ src/test/java/com/employee/SampleTest.java
@@ -0,0 +1,14 @@
+package com.employee;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest(){
+        assertTrue(true);
+    }
+}
